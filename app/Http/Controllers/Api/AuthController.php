35476594d62c2376cfd357589d92aa83115ed36a
dennisrmz <orellanadennis12@gmail.com>
2023-04-07 11:04:08 -0600

    update

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,38 +6,42 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
 
-        /* return env('APP_URL') . '/oauth/token'; */
-
-        $response = Http::withHeaders([
-            'Accept' => 'application/json',
-        ])->post(env('APP_URL') . '/oauth/token', [
-            'grant_type' => 'password',
-            'client_id' => $request->client_id,
-            'client_secret' => $request->client_secret,
-            'username' => $request->username,
-            'password' => $request->password,
-        ]);
-
-        $data = $response->json();
-
-        if ($response->status() === 200) {
+        // return env('APP_URL') . '/api/oauth/token';
+        // // timeout(10)->
+        // return [
+        //     'grant_type'    => 'password',
+        //     'client_id'     => $request->client_id,
+        //     'client_secret' => $request->client_secret,
+        //     'username'      => $request->username,
+        //     'password'      => $request->password,
+        // ];
+        // $response = Http::post(env('APP_URL') . '/oauth/token', [
+        //     'grant_type'    => 'password',
+        //     'client_id'     => $request->client_id,
+        //     'client_secret' => $request->client_secret,
+        //     'username'      => $request->username,
+        //     'password'      => $request->password,
+        // ]);
+        // $data = $response->json();
+        
+        // if ($response->status() === 200) {
 
             $user = User::where('email', $request->username)->first();
 
             return response()->json([
                 'user' => $user,
-                'token' => $data,
             ], 200);
 
-        }else{
-            return response()->json($data, $response->status());
-        }
+        // }else{
+        //     return response()->json($data, $response->status());
+        // }
 
         /* return $response->status(); */
 
